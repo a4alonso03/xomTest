a5e843dbd39b82a1e9c0a561476d45e521ebc256
Alonso Garita <alonso.garita1@gmail.com>
2017-06-19 21:07:28 -0600

    version 0.2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@
 public class Main {
     public static void main(String[] args) {
         ExerciseDataLoader loader = new ExerciseDataLoader();
-        //loader.load();
+        loader.load();
         System.exit(0);
 
     }
